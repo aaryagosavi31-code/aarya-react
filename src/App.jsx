0f94fd1d4f4196a0a288003fd8575deb1cd64edb
AaryaG <aaryagosavi31@gmail.com>
2026-04-06 21:00:10 +0530

    made footer-header-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { use, useState } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
-
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 function App() {
   const router = createBrowserRouter([
     {
@@ -18,7 +19,9 @@ function App() {
 
   return (
     <>
-     
+      <Navbar />
+      
+      <Footer />     
     </>
   )
 }
